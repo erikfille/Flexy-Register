46cce97ea45597d54b6371995ea103ba40daba18
erikfille <109306990+erikfille@users.noreply.github.com>
2023-06-04 12:31:40 -0300

    Browser Router Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import Login from "./views/Login/Login";
+import InfoModal from "./components/InfoModal";
+import "./App.css";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === "/") navigate("login");
+  }, [window.location]);
 
   return (
     <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Login />} />
+      </Routes>
+      <InfoModal />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
