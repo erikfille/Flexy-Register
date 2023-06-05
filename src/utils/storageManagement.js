23f1ed12941cede6050919401f26e3db26d61313
erikfille <109306990+erikfille@users.noreply.github.com>
2023-06-05 08:29:00 -0300

    delete method added to storeManagement.js

--- src/utils/storageManagement.js
+++ src/utils/storageManagement.js
@@ -3,3 +3,6 @@ export const saveToStorage = (storageId, item) =>
 
 export const getFromStorage = (storageId) =>
   JSON.parse(window.sessionStorage.getItem(storageId));
+
+export const deleteFromStorage = (storageId) =>
+  window.sessionStorage.removeItem(storageId);
